01343b1153c0244535c2a08c9333b65bc15079ff
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-05-10 21:55:42 +0530

    initial commit with navbar ready project

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="container">
+      <nav>
+        <img src="/images/brand_logo.png" alt="" />
+
+        <ul>
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              menu
+            </a>
+          </li>
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              location
+            </a>
+          </li>
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              about
+            </a>
+          </li>
+          <li>
+            <a href="http://" target="_blank" rel="noopener noreferrer">
+              contact
+            </a>
+          </li>
+        </ul>
+        <button>login</button>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
