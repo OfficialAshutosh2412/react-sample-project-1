fff4ad48a639901401269b5617e750977ee2606a
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-05-13 15:28:15 +0530

    finallized

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 function App() {
   return (
-    <div className="container">
+    <>
       <Navbar />
       <Hero />
-    </div>
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Navbar() {
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+  const handleNavToggler = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  };
   return (
     <nav>
       <img src="/images/brand_logo.png" alt="" />
-
       <ul>
         <li>
           <a href="http://" target="_blank" rel="noopener noreferrer">
@@ -27,7 +30,35 @@ function Navbar() {
           </a>
         </li>
       </ul>
-      <button>login</button>
+      <div>
+        <button>login</button>
+        <button className="hamburger" onClick={handleNavToggler}>
+          <i className="fa-solid fa-caret-down"></i>
+        </button>
+      </div>
+      {/* mobile navbar */}
+      <ul className={isMobileNavOpen ? "mobile-navbar show" : "mobile-navbar"}>
+        <li>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            menu
+          </a>
+        </li>
+        <li>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            location
+          </a>
+        </li>
+        <li>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            about
+          </a>
+        </li>
+        <li>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            contact
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
